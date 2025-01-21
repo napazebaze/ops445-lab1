24d9aea7a38a6fa1aa3be4e11729b73b6a76d79b
Aime Parfait Azebaze Ngueya <napazebaze@myseneca.ca>
2025-01-21 15:29:02 -0500

    Completed lab1d.py with math operations

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Assign integer values to objects
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation and display the result
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
